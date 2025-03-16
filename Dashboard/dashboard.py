c2b7bf2101a77d7b59be349e2bc88f154e3b368d
Tanbelajarcoding <sulthanrafif@student.ub.ac.id>
2025-03-17 00:35:59 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -5,5 +5,26 @@ import matplotlib.pyplot as plt  # Untuk visualisasi grafik
 import seaborn as sns       # Untuk visualisasi statistik
 import streamlit as st      # Untuk streamlit
 
+# -------------------------------
+# Load Data
+# -------------------------------
+hour = pd.read_csv("https://raw.githubusercontent.com/Tanbelajarcoding/bikesharingproject/refs/heads/main/Data/hour.csv")
+# -------------------------------
+# Sidebar: Filter dan Pilihan Analisis
+# -------------------------------
+st.sidebar.header("Filter Data")
+analysis_option = st.sidebar.radio("Pilih Analisis", ("Overview Data", "Analisis per Jam", "Tren Bulanan", "Peak Season"))
+
+# -------------------------------
+# Header
+# -------------------------------
 st.header('Dashboard Penyewaan Sepeda :sparkles:')
 st.caption("Last updated: 1 January 2013")
+
+# -------------------------------
+# Overview Data
+# -------------------------------
+if analysis_option == "Overview Data":
+    st.header("Overview Data")
+    st.subheader('Total Penyewaan')
+
