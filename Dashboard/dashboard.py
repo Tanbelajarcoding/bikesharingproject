59a0feee8200a1b3d70854830125ae466eb7ed69
Tanbelajarcoding <sulthanrafif@student.ub.ac.id>
2025-03-17 00:41:44 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -8,12 +8,12 @@ import streamlit as st      # Untuk streamlit
 # -------------------------------
 # Load Data
 # -------------------------------
-hour = pd.read_csv("https://raw.githubusercontent.com/Tanbelajarcoding/bikesharingproject/refs/heads/main/Data/hour.csv")
+hour = pd.read_csv("https://raw.githubusercontent.com/Tanbelajarcoding/bikesharingproject/refs/heads/main/Dashboard/hour_df.csv")
 # -------------------------------
 # Sidebar: Filter dan Pilihan Analisis
 # -------------------------------
 st.sidebar.header("Filter Data")
-analysis_option = st.sidebar.radio("Pilih Analisis", ("Overview Data", "Analisis per Jam", "Tren Bulanan", "Peak Season"))
+analysis_option = st.sidebar.radio("Pilih Analisis", ("Hubungan Humidity dan Jumlah Penyewaan Sepeda pada Musim Panas", "Perbandingan Jumlah Penyewaan Sepeda pada Hari Libur dan Hari Kerja", "pola penggunaan sepeda pada musim panas dan musim dingin"))
 
 # -------------------------------
 # Header
@@ -24,7 +24,17 @@ st.caption("Last updated: 1 January 2013")
 # -------------------------------
 # Overview Data
 # -------------------------------
-if analysis_option == "Overview Data":
-    st.header("Overview Data")
-    st.subheader('Total Penyewaan')
+if analysis_option == "Hubungan Humidity dan Jumlah Penyewaan Sepeda pada Musim Panas":
+    st.header("Hubungan Humidity dan Jumlah Penyewaan Sepeda pada Musim Panas")
+    
+    filtered_hour = hour[hour['season'].isin(['Summer'])]
+
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(data=filtered_hour, x='humidity', y='count', alpha=0.6, color='blue')
+    plt.title('Hubungan antara Kelembapan (Humidity) dan Jumlah Penyewaan Sepeda pada Musim Panas', fontsize=14)
+    plt.xlabel('Kelembapan (Humidity)', fontsize=12)
+    plt.ylabel('Jumlah Penyewaan Sepeda (cnt)', fontsize=12)
+    plt.grid(True)
+    plt.show()
+    
 
