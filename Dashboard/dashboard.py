3f01864daa363bf94425a2f53a91ef0efe794815
Tanbelajarcoding <sulthanrafif@student.ub.ac.id>
2025-03-17 00:13:40 +0700

    Create dashboard.py

---
+++ Dashboard/dashboard.py
@@ -0,0 +1,9 @@
+# Import library yang digunakan untuk analisis data dan visualisasi
+import pandas as pd         # Untuk manipulasi dan analisis data
+import numpy as np          # Untuk operasi numerik dan manipulasi data
+import matplotlib.pyplot as plt  # Untuk visualisasi grafik
+import seaborn as sns       # Untuk visualisasi statistik
+import streamlit as st      # Untuk streamlit
+
+st.header('Dashboard Penyewaan Sepeda :sparkles:')
+st.caption("Last updated: 1 January 2013")
